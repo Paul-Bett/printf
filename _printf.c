38a07248ae6cb027229d9a905fdbb2f5f04a1668
Paul-Bett <bettpaul6@gmail.com>
2022-07-11 14:17:58 +0300

    First commit

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _printf - Print based on format specifier
+ * @format - Specifier argument
+ * Return - Integer value
+ **/
+
+int _printf(const char *format, ...)
+{
+
+}
