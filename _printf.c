de73eb80267a7a1a956999618698ba5185188381
Stephy <atienostephanie040@gmail.com>
2022-07-11 10:21:28 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -5,23 +5,23 @@
  * @str* Array pointer
  * Return: Array pointer
  **/
-char *_strrev (char *str)
+char *_strrev(char *str)
 {
-  int i;
-  int len = 0;
-  char c;
-  if (!str)
-    return NULL;
-  while(str[len] != '\0'){
-    len++;
-  }
-  for(i = 0; i < (len/2); i++)
-  {
-    c = str[i];
-    str [i] = str[len - i - 1];
-    str[len - i - 1] = c;
-  }
-  return str;
+	int i;
+	int len = 0;
+	char c;
+	
+	if (!str)
+		return (NULL);
+	while (str[len] != '\0')
+		len++;
+	for (i = 0; i < (len / 2); i++)
+	{
+		c = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = c;
+	}
+	return (str);
 }
 /**
  * _itoa - Character to Array
@@ -29,26 +29,28 @@ char *_strrev (char *str)
  * @base: Base of number
  * Return: Array pointer
  **/
-char * _itoa(int i, char *strout, int base)
+char *_itoa(int i, char *strout, int base)
 {
-  char *str = strout;
-  int digit, sign = 0;
-  if (i < 0) {
-    sign = 1;
-    i *= -1;
-  }
-  while(i) {
-    digit = i % base;
-    *str = (digit > 9) ? ('A' + digit - 10) : '0' + digit;
-    i = i / base;
-    str ++;
-  }
-  if(sign) {
-  *str++ = '-';
-  }
-  *str = '\0';
-  _strrev(strout);
-  return strout;
+	char *str = strout;
+	int digit, sign = 0;
+	
+	if (i < 0)
+	{
+		sign = 1;
+		i *= -1;
+	}
+	while (i)
+	{
+		digit = i % base;
+		*str = (digit > 9) ? ('A' + digit - 10) : '0' + digit;
+		i = i / base;
+		str++;
+	}
+	if (sign)
+		*str++ = '-';
+	*str = '\0';
+	_strrev(strout);
+	return (strout);
 }
 
 /**
@@ -88,7 +90,7 @@ int _printf(const char *format, ...)
 		}
 	/* Convert decimal */
 			case 'd': {
-				_itoa(va_arg( vl, int ), tmp, 10);
+				_itoa(va_arg(vl, int), tmp, 10);
 				strcpy(&buff[j], tmp);
 				j += strlen(tmp);
 			break;
